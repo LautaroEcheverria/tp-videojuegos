4005ca3d24ec885576ea61158499b338cc724542
LautaroEcheverria <lauta.e.96@gmail.com>
2021-06-06 19:53:43 -0300

    Delete nivelRitmico.gd

--- producto/scripts/nivelRitmico.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _ready():
-	
-	# Posicion ventana reproduccion
-	var screen_size = OS.get_screen_size(OS.get_current_screen())
-	var window_size = OS.get_window_size()
-	var centered_pos = (screen_size - window_size) / 2
-	OS.set_window_position(centered_pos)
