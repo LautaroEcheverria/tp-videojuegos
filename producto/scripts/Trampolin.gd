329d2fbb22c3c7d0cf72bd0b3f007b033d50a112
ampieroni <42614620+ampieroni@users.noreply.github.com>
2021-06-20 18:30:34 -0300

    actualizacion escena trampolines

--- producto/scripts/Trampolin.gd
+++ producto/scripts/Trampolin.gd
@@ -10,6 +10,7 @@ var velocidad  = 24.0
 
 #"Propiedad" de seguimiento
 var follow = Vector2.ZERO
+var previo_follow
 
 #Guarda la posicion de la trampolin y del nodo Tween
 onready var trampolin = $TrampolinBody
@@ -29,30 +30,42 @@ func _iniciar_tween():
 	move_a = move_a * cell_size
 	var duracion = move_a.length()/velocidad
 	#IDA
-	tween.interpolate_property(self, "follow", Vector2.ZERO, 
-							   move_a, duracion, Tween.TRANS_LINEAR,
-							   Tween.EASE_IN_OUT, idle_duration)
+	if (state == State.Move_down):
+		tween.interpolate_property(self, "follow", Vector2.ZERO, 
+								   move_a, duracion, Tween.TRANS_LINEAR,
+								   Tween.EASE_IN_OUT, idle_duration)
 	#VUELTA
-	tween.interpolate_property(self, "follow", move_a, Vector2.ZERO, 
+	if (state == State.Move_up):
+		tween.interpolate_property(self, "follow", move_a, Vector2.ZERO, 
 							   duracion, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 
 							   duracion + idle_duration * 2)
 	tween.start()
 
 func _physics_process(delta):
 	#seguimiento de posicion
+	previo_follow = follow
 	trampolin.position = trampolin.position.linear_interpolate(follow, 0.075)
+	
 	#animacion
-	var cero = Vector2(0,0)
-	if (follow == cero):
-		print("alto")
+	if (state == State.Idle_up):
 		if $TrampolinBody/SpriteTrampolin.animation != "Idle_up":
 			$TrampolinBody/SpriteTrampolin.play("Idle_up")
-	elif (follow == move_a):
-		print("bajo")
+	if (state == State.Idle_down):
 		if $TrampolinBody/SpriteTrampolin.animation != "Idle_down":
 			$TrampolinBody/SpriteTrampolin.play("Idle_down")
-	elif (follow == medio):
-		print("medio")
-		if $TrampolinBody/SpriteTrampolin.animation != "Move":
-			$TrampolinBody/SpriteTrampolin.play("Move")
-		
+	if (state == State.Move_down):
+		if $TrampolinBody/SpriteTrampolin.animation != "Move_down":
+			$TrampolinBody/SpriteTrampolin.play("Move_down")
+		if ($TrampolinBody/SpriteTrampolin.frame == 4):
+			state = State.Idle_down
+	if (state == State.Move_up):
+		if $TrampolinBody/SpriteTrampolin.animation != "Move_up":
+			$TrampolinBody/SpriteTrampolin.play("Move_up")
+		if ($TrampolinBody/SpriteTrampolin.frame == 4):
+			state = State.Idle_up
+
+func _down():
+	state = State.Move_down
+
+func _up():
+	state = State.Move_up
