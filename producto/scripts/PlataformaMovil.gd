25529bfdae47fb2069529d3660f26300a6ff18e3
ampieroni <42614620+ampieroni@users.noreply.github.com>
2021-06-21 15:32:18 -0300

    Plataformas Moviles - posibilidad activar desactivar

--- producto/scripts/PlataformaMovil.gd
+++ producto/scripts/PlataformaMovil.gd
@@ -5,8 +5,9 @@ export var idle_duration = 2.0
 export var move_a = Vector2(24,0)
 export var cell_size = Vector2(24,24)
 export var velocidad  = 64.0
-export var ascensor = false
+export var ascensor = true
 
+export var activo = false
 #"Propiedad" de seguimiento
 var follow = Vector2.ZERO
 var previous_follow = Vector2.ZERO
@@ -21,43 +22,49 @@ func _iniciar_tween():
 	move_a = move_a * cell_size
 	var duracion = move_a.length()/velocidad
 	#IDA
-	
 	tween.interpolate_property(self, "follow", Vector2.ZERO, 
-							   move_a, duracion, Tween.TRANS_LINEAR,
-							   Tween.EASE_IN_OUT, idle_duration)
+								   move_a, duracion, Tween.TRANS_LINEAR,
+								   Tween.EASE_IN_OUT, idle_duration)
 	
 	#VUELTA
 	tween.interpolate_property(self, "follow", move_a, Vector2.ZERO, 
-							   duracion, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 
-							   duracion + idle_duration * 2)
+								   duracion, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 
+								   duracion + idle_duration * 2)
 	
 	tween.start()
-
+	
 func _physics_process(delta):
 	
-	#para el flip
-	if ascensor == true:
-		previous_follow.y = plataforma.position.y
-	else:
-		previous_follow.x = plataforma.position.x
+	if activo:
+		if (tween.pause_mode):
+			tween.start()
+		#para el flip
+		if ascensor == true:
+			previous_follow.y = plataforma.position.y
+		else:
+			previous_follow.x = plataforma.position.x
 
-	#seguimiento de posicion
-	plataforma.position = plataforma.position.linear_interpolate(follow, 0.075)
-	
-	#para el flip
-	if ascensor == true:
-		previous_follow.x = plataforma.position.x
-	else:
-		previous_follow.y = plataforma.position.y
-	
-	#flipeo
-	if previous_follow > follow:
-		$Plataforma/SpritePlataforma.flip_h = false
-	else:
-		$Plataforma/SpritePlataforma.flip_h = true
+		#seguimiento de posicion
+		plataforma.position = plataforma.position.linear_interpolate(follow, 0.075)
 		
-	#animacion
-	if (follow == Vector2.ZERO) or (follow == move_a):
-		$Plataforma/SpritePlataforma.play("Idle")
+		#para el flip
+		if ascensor == true:
+			previous_follow.x = plataforma.position.x
+		else:
+			previous_follow.y = plataforma.position.y
+		
+		#flipeo
+		if previous_follow > follow:
+			$Plataforma/SpritePlataforma.flip_h = false
+		else:
+			$Plataforma/SpritePlataforma.flip_h = true
+			
+		#animacion
+		if (follow == Vector2.ZERO) or (follow == move_a):
+			$Plataforma/SpritePlataforma.play("Idle")
+		else:
+			$Plataforma/SpritePlataforma.play("Move")
 	else:
-		$Plataforma/SpritePlataforma.play("Move")
+		if $Plataforma/SpritePlataforma.animation != "Idle":
+			$Plataforma/SpritePlataforma.play("Idle")
+		tween.stop(plataforma,"")
